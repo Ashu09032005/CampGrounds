a0ba0df33169618dd6782f0e9f9cd390712dcfe6
Ashu09032005 <ashita9305@gmail.com>
2025-03-04 15:04:45 +0530

    Added cloudinary

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -16,8 +20,8 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+
+
 });
 
 const db = mongoose.connection;
--- seeds/index.js
+++ seeds/index.js
@@ -14,15 +14,30 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        console.log("check")
         const camp = new Campground({
             author: '67c143a9a2f7e42117ebec9c',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,//city array passed
             title: `${sample(descriptors)} ${sample(places)}`,//desc n places array is passed
-            image: `https://picsum.photos/400?random=${Math.random()}`,
+
+
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-            price: Math.floor(Math.random() * 20) + 10
+            price: Math.floor(Math.random() * 20) + 10,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dhhyqzxnr/image/upload/v1741066913/YelpCamp/hwvq5kxwi6s5mnz0hyih.jpg',
+                    filename: 'YelpCamp/hwvq5kxwi6s5mnz0hyih',
+
+                },
+                {
+                    url: 'https://res.cloudinary.com/dhhyqzxnr/image/upload/v1741066913/YelpCamp/rms2hogxc7yh3kg2yi8f.jpg',
+                    filename: 'YelpCamp/rms2hogxc7yh3kg2yi8f',
+
+                }
+            ]
 
         })
+        console.log("Saving campground:", camp);
         await camp.save();
 
     }
